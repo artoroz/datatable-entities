df14999b2e5259c784b52cc579d56cb6ba0b16e0
wimski <wimreckman@gmail.com>
2022-07-05 12:49:10 +0200

    Fix DatatableTrait

--- src/DatatableTrait.php
+++ src/DatatableTrait.php
@@ -1,18 +1,19 @@
 <?php
 namespace Artoroz\Datatable;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Artoroz\Datatable\Response\DatatableResponse;
-use Artoroz\Datatable\Table;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 trait DatatableTrait
 {
     protected abstract function getEntityManager(): EntityManagerInterface;
+    protected abstract function getUser(): ?UserInterface;
 
     protected function createTable($tableClass, $entityClass, Request $request, $options = []): Table
     {
-       // todo create factory for DI
-        $em = isset($options['em']) ? $options['em'] : $this->entityManager;
+       /** @var EntityManagerInterface $em */
+        $em = $options['em'] ?? $this->getEntityManager();
 
         if ($entityClass instanceof DatatableRepositoryInterface) {
             $repository = $entityClass;
